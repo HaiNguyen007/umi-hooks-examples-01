9e05fdd159938a539d733edd3fc9afe9f05ca29c
Hai Nguyen <HaiNguyen007@users.noreply.github.com>
2022-01-21 01:08:20 +0700

    linkback from codesandbox to Github

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default () => {
   return (
     <div>
-      <h1>Umi Hooks template</h1>
+      <h1>Umi Hooks template!</h1>
       <p>put your code here</p>
     </div>
   );
