ea55c3e67cd81d6bb5278e23fc0fa3e0e24a7916
Hai Nguyen <HaiNguyen007@users.noreply.github.com>
2022-01-21 01:14:40 +0700

    success created component UseToggle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
 import UseToggle from "./UseToggle";
 export default () => {
-  return (
-    <div>
-      <h1>Umi Hooks template!</h1>
-      <p>put your code here</p>
-    </div>
-  );
+  return <UseToggle />;
 };
